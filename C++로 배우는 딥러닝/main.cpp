d66a37ffcda920eb14c21920e91d62745deea61f
leeyunhome <leeyunhome@daum.net>
2021-10-04 09:11:17 +0900

    딥러닝 3. C++ Programming Feed-forward (한국어) 각각의 입력에 대해서 뉴런이 어떤 값을 내놓는지 w_값과 b_값을 어떻게 구하는지

--- C++로 배우는 딥러닝/main.cpp
+++ C++로 배우는 딥러닝/main.cpp
@@ -1,8 +1,44 @@
 ﻿#include <iostream>
 
+#define MAX2(a, b) (a) > (b) ? (a) : (b)
+
+class Neuron
+{
+public:
+    double w_;  // weight of one input
+    double b_;  // bias
+
+    double getAct(const double& x)
+    {
+        // for linear or identity activation functions
+        return x;
+
+        // for ReLU activation functions
+        //return MAX2(0.0, x);
+    }
+    double feedForward(const double& input)
+    {
+        // output y = f(\sigma)
+        // \sigma = w_ * input x + b
+        // /for multiple inputs,
+        // \sigma = w0_ * x0_ + w1_ * x1 + ... + b
+
+        const double sigma = w_ * input + b_;
+
+        return getAct(sigma);
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    Neuron my_neuron;
+    my_neuron.w_ = 2.0;
+    my_neuron.b_ = 1.0;
+
+    std::cout << "Input = 0.0 " << my_neuron.feedForward(0.0) << std::endl;
+    std::cout << "Input = 1.0 " << my_neuron.feedForward(1.0) << std::endl;
+    std::cout << "Input = 2.0 " << my_neuron.feedForward(2.0) << std::endl;
+    std::cout << "Input = 3.0 " << my_neuron.feedForward(3.0) << std::endl;
 
     return 0;
 }
