139b94132041fedb49b3ad8ed36533291d93577b
leeyunhome <leeyunhome@daum.net>
2021-10-04 10:46:37 +0900

    C-style to C++ (OOP)

--- c-style_feedForward/main.cpp
+++ c-style_feedForward/main.cpp
@@ -1,68 +1,67 @@
-// C-style implementation of the feed-forward
+// C-style to C++ (OOP) implementation of the feed-forward
+// 어떤 행위를 할 때 거기에 필요한 최소한의 데이터가 있겠죠?
+// 뭔가 컴퓨터가 뭔가 작업을 하려면 입력을 받고 자기의 속성에 맞춰서
+// 작업을 하고 출력을 내놓는 그런 방식이죠?
+// 그래서 객체를 선언을 해줄 때는 행위와 데이터를 묶어 줍니다.
+
 #include <stdio.h>
 #define MAX2(a, b) (a) > (b) ? (a) : (b)
 
-double getActivation(const double x) // c
+class Neuron
 {
-	// linear or identity activation function
-	return x;
+public:
+	Neuron()
+		: w_(2.0), b_(1.0)
+	{}
 
-	// ReLU max(0, x);
-	// return MAX2(0, x);
-}
+	Neuron(const double& w_input, const double& b_input)
+		: w_(w_input), b_(b_input)
+	{}
 
-// attributes of a neuron
-double w;	// weight of an input
-double b;	// /bias
+public: // attributes
+	double w_;	// weight of an input
+	double b_;	// /bias
 
-// 뉴런에 따라서 feedForward가 어떤 값을 리턴할지가 달라지는 거예요.
-// 각각의 뉴런에대해서
-// 그래서 결국 구현을 하고 다듬다보면 객체지향으로 가게됩니다.
-double feedForward(const double input)
-{
-	const double sigma = w * input + b;
+public:	// behaviors
+	double feedForward(const double& input)
+	{
+		const double sigma = w_ * input + b_;
 
-	// linear or identity activation function f(x) = x
-	const double activation = getActivation(sigma);	// identity act f(x) = x;
-	//double activation = f(sigma);
+		return getActivation(sigma);
+	}
 
-	const double output = activation;
+	double getActivation(const double& x) // c
+	{
+		// linear or identity activation function
+		return x;
 
-	//return output;
-	return getActivation(sigma);
-}
+		// ReLU max(0, x);
+		// return MAX2(0, x);
+	}
+
+	void feedForwardAndPrint(const double& input)
+	{
+		printf("%f %f \n", input, feedForward(input));
+	}
+};
 
 int main()
 {
-	// initialize attributes of neuron
-	// w = ?
-	// b = ?
+	// initialize my_neuron
+	// 어차피 나중에는 요값들을 훈련을 시켜서 자기가 결정하도록
+	// 만들어 줍니다.
+	Neuron my_neuron(2.0, 1.0);
 
-	w = 2.0;
-	b = 1.0;
+	for (double input = 0.0; input < 3.0; input += 1.0)
+		my_neuron.feedForwardAndPrint(input);
 
-	double input;	// input x
-	double output;
-
-	input = 0.0;
-	output = feedForward(input);
-	printf("%f %f \n", input, output);
-
-	input = 1.0;
-	output = feedForward(input);
-	printf("%f %f \n", input, output);
-
-	input = 2.0;
-	output = feedForward(input);
-	printf("%f %f \n", input, output);
-
-	// output y = f(\sigma) = f(w*input x + b)
-	// 가장먼저 코딩을 할 때는 가장 먼저 작은 거 부터 해야되겠죠?
-	// x같은 많이 쓰이는 짧은 변수명 보다는
-	// 이렇게 직관적으로 의미를 알 수 있는 input 요런 이름을 써주는 게 좋습니다.
-
-
-	//double output = activation;
+	//my_neuron.feedForwardAndPrint(0.0);
+	//my_neuron.feedForwardAndPrint(1.0);
+	//my_neuron.feedForwardAndPrint(2.0);
+	
+	// OOP를 사용을 하면은 나중에 사용을 할 때 굉장히 편합니다.
+	// 반복적으로 사용할 부분은 가급적으로 이렇게 OOP를 사용해서
+	// 정리를 하는 게 좋겠죠?
 
 	return 0;
 }
