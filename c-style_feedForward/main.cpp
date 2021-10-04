33b8ddb85f16568f82107e83f5b78b69ae0002e2
leeyunhome <leeyunhome@daum.net>
2021-10-04 18:33:18 +0900

    C-style to C++ (OOP)

--- c-style_feedForward/main.cpp
+++ c-style_feedForward/main.cpp
@@ -5,6 +5,7 @@
 // 그래서 객체를 선언을 해줄 때는 행위와 데이터를 묶어 줍니다.
 
 #include <stdio.h>
+#include <iostream>
 #define MAX2(a, b) (a) > (b) ? (a) : (b)
 
 class Neuron
@@ -22,21 +23,45 @@ public: // attributes
 	double w_;	// weight of an input
 	double b_;	// /bias
 
-public:	// behaviors
-	double feedForward(const double& input)
-	{
-		const double sigma = w_ * input + b_;
+	double input_, output_; // saved for back-prop
 
-		return getActivation(sigma);
+public:	// behaviors
+	double feedForward(const double& _input)
+	{
+		input_ = _input;
+
+		const double sigma = w_ * _input + b_;
+
+		output_ = getAct(sigma);
+
+		return output_;
 	}
 
-	double getActivation(const double& x) // c
+	double getAct(const double& x) // c
 	{
 		// linear or identity activation function
 		return x;
 
 		// ReLU max(0, x);
 		// return MAX2(0, x);
+	}
+
+	void propBackward(const double& target)
+	{
+		const double alpha = 0.1; // learning rate
+
+		const double grad = (output_ - target) * getActGrad(output_);
+
+		w_ -= alpha * grad * input_; // last input_ came from d(wx+b)/dw = x
+		b_ -= alpha * grad * 1.0;	 // last 1.0 came from d(wx+b)/db = 1
+	}
+
+	double getActGrad(const double& x)
+	{
+		// linear or identity activation function
+		return 1.0;
+
+		// ReLU if (x > 0.0) return 1.0; else 0.0;
 	}
 
 	void feedForwardAndPrint(const double& input)
@@ -47,21 +72,17 @@ public:	// behaviors
 
 int main()
 {
+	using namespace std;
 	// initialize my_neuron
-	// 어차피 나중에는 요값들을 훈련을 시켜서 자기가 결정하도록
-	// 만들어 줍니다.
 	Neuron my_neuron(2.0, 1.0);
 
-	for (double input = 0.0; input < 3.0; input += 1.0)
-		my_neuron.feedForwardAndPrint(input);
-
-	//my_neuron.feedForwardAndPrint(0.0);
-	//my_neuron.feedForwardAndPrint(1.0);
-	//my_neuron.feedForwardAndPrint(2.0);
-	
-	// OOP를 사용을 하면은 나중에 사용을 할 때 굉장히 편합니다.
-	// 반복적으로 사용할 부분은 가급적으로 이렇게 OOP를 사용해서
-	// 정리를 하는 게 좋겠죠?
+	for (int r = 0; r < 100; r++)
+	{
+		std::cout << "Training " << r << std::endl;
+		my_neuron.feedForwardAndPrint(1.0);
+		my_neuron.propBackward(4.0);
+		std::cout << "w = " << my_neuron.w_ << " b = " << my_neuron.b_ << std::endl;
+	}
 
 	return 0;
 }
